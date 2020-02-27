844d6524bf232d1ff74e2df3b2489c804f5d7fa9
Bibek Bhandari <Cixwarebibek@gmail.com>
2020-02-27 10:43:00 +0545

    sort array using pre defined and userdefined function

--- assending-array.php
+++ assending-array.php
@@ -18,7 +18,7 @@
   
 ?>
 <!-- 
-Sorting array in assending order -->
+Sorting array in assending order using predefined function-->
 
 <!DOCTYPE html>
 <html>
@@ -36,6 +36,44 @@ for($i=0;$i<$arrlength;$i++)
   echo "<br>";
   }
 ?>
+<!-- sorting array using user defined funcrion -->
 
 </body>
 </html>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<br>";
+$array =['5','3','1','4','2'];
+
+function assending_array($array){
+  // $sort=[];
+  $count = count($array);
+  for ($i = 0; $i < $count - 1; $i++)
+ {
+  for($i=0; $i<$count-1; $i++){
+    for($j=0; $j<$count-1-$i; $j++)
+    {
+        if($array[$j] > $array[$j+1])
+          {
+              $temp = $array[$j];
+              $array[$j]=$array[$j+1];
+              $array[$j+1]=$temp;                
+          }         
+    }      
+}
+  return $array;
+ }
+ 
+} 
+$assendingOrder=assending_array($array);
+print_r($assendingOrder);
+?>
+
+</body>
+</html>
+
+
